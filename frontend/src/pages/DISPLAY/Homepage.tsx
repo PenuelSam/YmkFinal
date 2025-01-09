3e287f340c0ab21d0d8595125b46b102099b2778
PenuelSam <penuelsamuel54@gmail.com>
2025-01-09 01:14:41 +0100

    commit

--- frontend/src/pages/DISPLAY/Homepage.tsx
+++ frontend/src/pages/DISPLAY/Homepage.tsx
@@ -7,13 +7,13 @@ export const Homepage = () => {
   return (
     <div >
     <Index />
-    <div className="w-full h-full flex md:flex-col items-center justify-center px-[3.5rem] mt-[3rem] gap-32">
+    <div className="w-full h-full flex md:flex-col items-center justify-center px-[3.5rem] md:px-[2rem] mt-[3rem] gap-32">
         <div className="flex-1  border mt-32 ">
           <img src={image} alt="" className="w-full h-full object-cover"/>
         </div>
         <div className="flex-1  flex flex-col gap-10 md:gap-5">
           <p className="text-[20px] font-Inter">FullStack Creative.</p>
-          <p className="text-[4rem] md:text-[2rem] leading-[80px] md:leading-[1.6] pr-[2rem] md:pr-0 font-Inter tracking-tight border font-semibold" >Hi, my name is Yimika. I do photography, videography and content marketing. Let’s work together.</p>
+          <p className="text-[4rem] md:text-[2rem] leading-[80px] md:leading-[1.6] pr-[2rem] md:pr-0 font-Inter tracking-tight font-semibold" >Hi, my name is Yimika. I do photography, videography and content marketing. Let’s work together.</p>
           <Link to="/contact" className="mt-20 md:mt-5">
             <button className="px-[2rem] py-[1rem] bg-black text-white rounded-md text-[18px]">Contact</button>
           </Link>
