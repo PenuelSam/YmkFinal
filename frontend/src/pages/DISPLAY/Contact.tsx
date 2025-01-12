c9b87ea9cc829bfc01e89d0f5fd7c494a359a98c
PenuelSam <penuelsamuel54@gmail.com>
2025-01-12 03:56:14 +0100

    commit

--- frontend/src/pages/DISPLAY/Contact.tsx
+++ frontend/src/pages/DISPLAY/Contact.tsx
@@ -1,24 +1,39 @@
-import { IoLogoInstagram } from "react-icons/io5";
+import { IoLogoInstagram, IoMdMail } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { Index } from "../../component/nav";
 
 export const Contact = () => {
   return (
     <div className="">
-        <Index />
-       <div className="flex flex-col gap-10  ml-10 my-[20rem]">
-       <h1 className="text-[5rem] md:text-[2rem] uppercase font-Inter ">Contact Me</h1>
+      <Index />
+      <div className="flex flex-col gap-10  ml-10 my-[20rem]">
+        <h1 className="text-[5rem] md:text-[2rem] uppercase font-Inter ">
+          Contact Me
+        </h1>
         <div className="flex flex-col gap-10">
-            <p className="font-Inter text-[1.5rem] md:text-[1rem] sm:text-[0.8rem] xsm:text-[0.5rem]">YMKUPNEXT! IS YOUR FAVOURITE DIRECTOR'S FAVOURITE DIRECTOR</p>
-            <div className="flex items-center gap-5">
-            <p className="text-[1.5rem] md:text-[1rem] sm:text-[0.8rem] xsm:text-[0.5rem] font-Inter">contactymk30@gmail.com</p>
+          <p className="font-Inter text-[1.5rem] md:text-[1rem] sm:text-[0.8rem] xsm:text-[0.5rem]">
+            YMKUPNEXT! IS YOUR FAVOURITE DIRECTOR'S FAVOURITE DIRECTOR
+          </p>
+          <div className="flex items-center gap-5">
+            <p className="text-[1.5rem] md:text-[1rem] sm:text-[0.8rem] xsm:text-[0.5rem] font-Inter">
+              contactymk30@gmail.com
+            </p>
             <div className="w-[0.5px] h-10 md:h-5 bg-white"></div>
-           <Link to="https://www.instagram.com/ymkupnext" target="_blank">
-           <IoLogoInstagram className="text-[30px] md:text-[20px]"/>
-           </Link>
-            </div>
+            <Link
+              to="mailto:contactymk30@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-5 border-2 border-black px-3 py-1 rounded-full"
+            >
+              <p>click  to send me a mail</p>
+              <IoMdMail className="text-[30px] md:text-[20px]" />
+            </Link>
+            <Link to="https://www.instagram.com/ymkupnext" target="_blank">
+              <IoLogoInstagram className="text-[30px] md:text-[20px]" />
+            </Link>
+          </div>
         </div>
-       </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- frontend/src/pages/DISPLAY/Homepage.tsx
+++ frontend/src/pages/DISPLAY/Homepage.tsx
@@ -8,7 +8,7 @@ export const Homepage = () => {
     <div >
     <Index />
     <div className="w-full h-full flex md:flex-col  items-center justify-center px-[3.5rem] md:px-[2rem] mt-[3rem] gap-32 md:gap-5">
-        <div className=" w-[50%]  md:w-[100%] md:h-[600px] sm:h-[460px]">
+        <div className=" w-[50%]  md:w-[100%] md:h-[600px] sm:h-[500px]">
           <img src={image} alt="" className="w-full h-full object-cover"/>
         </div>
         <div className=" w-[50%] md:w-[100%] flex flex-col  gap-5">
